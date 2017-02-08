ad3fb46d7c8f2be3831d5ce2150e78dbaa104470
Matthew Gardiner <M.Gardiner2@bradford.ac.uk>
2017-02-08 14:14:46 +0000

    exercises 1 through 8

---
+++ ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*This is a comment*/
+int main(int argc, char *argv[]) 
+{
+    int distance = 100;
+
+    //This too is a comment
+    printf("you are %d miles away.\n", distance);
+
+    return 0;
+}
---
+++ ex8.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) 
+{
+    int areas[] = {10,12,13,14,20};
+    char name[] = "Zed";
+    char full_name[] = {
+        'Z','e','d',
+        ' ','A','.',' ',
+        'S','h','a','w','\0'
+    };
+
+    //This may not work, you may have to change from %ld to %u
+
+    printf("The size of an int: %ld \n",
+        sizeof(int));
+    printf("The size of areas (int[]): %ld \n", 
+        sizeof(areas));
+    printf("The number of int in areas: %ld \n",
+        sizeof(areas) / sizeof(int));
+    printf("The first area is %d, the second is %d. \n",
+        areas[0], areas[1]);
+
+    printf("The size of a char: %ld\n", sizeof(char));
+    printf("The size of a name (char[]): %ld\n",
+        sizeof(name));
+    printf("the number of chars: %ld \n",
+        sizeof(name) / sizeof(char));
+
+    printf("The size of full_name (char[]): %ld\n",
+        sizeof(full_name));
+    printf("The number of chars: %ld\n",
+        sizeof(full_name) / sizeof(char));
+
+    printf("name= \"%s\" and full_name = \"%s\"\n",
+        name, full_name);
+    return 0;
+}
